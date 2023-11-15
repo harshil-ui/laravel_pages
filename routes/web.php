c4d2992c8f9cf2432aafd7d26f3ee439fb1957e1
Harshil <har.harshil@outlook.com>
2023-11-15 18:36:51 +0530

    Add middleare

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::view('/register', 'auth.register')->name('register');
+Route::middleware('guest')->group(function () {
+    Route::view('/register', 'auth.register')->name('register');
 
-Route::post('/create-user', [UserController::class, 'register'])->name('create-user');
+    Route::view('/login', 'auth.login')->name('login');
+});
 
-Route::get('/dashboard', [UserController::class, 'dashboard'])->name('home');
 
-Route::view('/login', 'auth.login')->name('login');
+Route::controller(UserController::class)->group(function () {
 
-Route::post('/post-login', [UserController::class, 'login'])->name('post-login');;
+    Route::post('/create-user', 'register')->name('create-user');
+
+    Route::get('/', 'dashboard')->name('home')->middleware('auth');
+
+    Route::post('/post-login', 'login')->name('post-login');
+});
